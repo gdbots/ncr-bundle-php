d3ef00b48f0c6bbc9da90a0eec98ce0caa2481a1
Greg Brown <greg@12dnetworks.com>
2025-02-13 13:43:42 -0800

    v4.0.1 -- Handle Symfony deprecations.

--- src/DependencyInjection/GdbotsNcrExtension.php
+++ src/DependencyInjection/GdbotsNcrExtension.php
@@ -11,9 +11,9 @@ use Gdbots\Ncr\NcrSearch;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class GdbotsNcrExtension extends Extension
 {
